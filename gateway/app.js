28b49d2308482228f0a357c74ea8468bd76520c2
Eagan Martin <mece.martinece@gmail.com>
2019-03-02 00:47:49 +0800

    Reworked app as express

---
+++ gateway/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = require('./routers/router');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(router);
+
+module.exports = app;
