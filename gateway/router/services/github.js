e7549db01bb3094519e5d022a15d222a746045a4
Eagan Martin <mece.martinece@gmail.com>
2019-03-02 02:09:14 +0800

    Implemented rate limiter for api

--- gateway/router/services/github.js
+++ gateway/router/services/github.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const router = express.Router();
+const rateLimit = require('express-rate-limit');
 const adapter = require('../adapter');
+
+const router = express.Router();
 
 const BASE_URL = 'https://api.github.com';
 const api = adapter(BASE_URL);
 
-router.get('/users/pupupulp', (req, res) => {
+const apiLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 1 hour window
+	max: 5, // start blocking after 5 requests
+	message:
+		'Too many accounts created from this IP, please try again after an hour'
+});
+
+router.get('/users/pupupulp', apiLimiter, (req, res) => {
 	api.get(req.path).then(resp => {
 		res.send(resp.data)
 	});
