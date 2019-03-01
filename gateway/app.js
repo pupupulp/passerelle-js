33e7c3af03b81a6b784500b8e6ce7d73e04684ba
Eagan Martin <mece.martinece@gmail.com>
2019-03-02 03:09:11 +0800

    Implemented request id

--- gateway/app.js
+++ gateway/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const monitor = require('express-status-monitor')({ path: '' });
+const addRequestId = require('express-request-id');
 
 const secure = require('../server/security');
 const router = require('./router/bridge');
@@ -7,12 +8,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-
 secure(app);
 
 app.use(monitor.middleware);
 app.get('/status', monitor.pageRoute);
 
+app.use(addRequestId());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
