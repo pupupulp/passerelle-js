9197ea694d778b94e9f3cd1fd8de500f6900209f
Eagan Martin <mece.martinece@gmail.com>
2019-03-02 01:36:04 +0800

    Added false powered by for additional security

--- server/security.js
+++ server/security.js
@@ -17,5 +17,5 @@ module.exports = app => {
 
 	app.use(helmet.frameguard({ action: 'sameorigin' }));
 
-	app.use(helmet.hidePoweredBy());
+	app.use(helmet.hidePoweredBy({ setTo: 'PHP 7.3.0' }));
 };
