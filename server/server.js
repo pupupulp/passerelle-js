603ad2fbd3764400e4f6c97924de664cb416156f
Eagan Martin <mece.martinece@gmail.com>
2019-03-02 11:06:44 +0800

    Added port normalization

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const app = require('../gateway/app');
 const http = require('http');
 const logger = require('../logger/winston');
 
-const port = '9000';
+const port = normalizePort('9000');
 
 const server = http.createServer(app);
 server.listen(port);
@@ -33,3 +33,17 @@ server.on('listening', () => {
 	// eslint-disable-next-line no-console
 	logger.info('Listening on port ' + address.port);
 });
+
+function normalizePort(value) {
+	var port = parseInt(value, 10);
+
+	if(isNaN(port)) {
+		return value;
+	}
+
+	if(port >= 0) {
+		return port;
+	}
+
+	return false;
+}
